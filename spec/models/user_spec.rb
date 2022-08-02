802e2cf08092fd1766846e8f0bec74ea255786ca
Payam Beigi <beigi.payam@gmail.com>
2022-08-02 20:20:38 +0000

    install cypress

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,89 +74,89 @@ RSpec.describe User, type: :model do
     
           end
 
-          it 'Should return invalid if passwords do not match ' do
-
-            user = User.create({
-              first_name: 'Nelly',
-              last_name: 'Furtado',
-              email: 'nilly@gmail.com',
-              password: 'thisIsDifferent',
-              password_confirmation: '1234Nilly',
-            })
-
-            expect(user).to_not be_valid
-      
-            end
-
-            it 'Should not allow save if passwords do not match' do
-
-              user = User.create({
-                first_name: 'Nelly',
-                last_name: 'Furtado',
-                email: 'nilly@gmail.com',
-                password: 'thisIsDifferent',
-                password_confirmation: '1234Nilly',
-              })
-
-              expect(user.save).to_not be_truthy
-        
-              end
-
-              it 'Should not allow existing emails for registeration -Not case sensitive-' do
-
-                user1 = User.new({
-                  first_name: 'Nelly',
-                  last_name: 'Furtado',
-                  email: 'nilly@gmail.com',
-                  password: 'thePasssword',
-                  password_confirmation: 'thePasssword',
-                })
-                user1.save
-
-                user2 = User.new({
-                  first_name: 'Nelly',
-                  last_name: 'Furtado',
-                  email: 'NILLY@GMAIL.COM',
-                  password: 'thePasssword',
-                  password_confirmation: 'thePasssword',
-                })
-                expect(user2.save).to_not be_truthy
-          
-                end
-
-                it 'Should ignore spaces before or after Email input' do
-
-                  user = User.new({
-                    first_name: 'Nelly',
-                    last_name: 'Furtado',
-                    email: '  nilly@gmail.com ',
-                    password: 'thePasssword',
-                    password_confirmation: 'thePasssword',
-                  })
-    
-                  expect(user.save).to be_truthy
-
-            
-                end
-
-                it 'Password should have a minimum length of 5' do
-
-                  user = User.new({
-                    first_name: 'Nelly',
-                    last_name: 'Furtado',
-                    email: 'nilly@gmail.com',
-                    password: '1234',
-                    password_confirmation: '1234',
-                  })
-    
-                  expect(user).to_not be_valid
-
-            
-                end
 end
 
   describe '.authenticate_with_credentials' do
   
-  end
+    it 'Should return invalid if passwords do not match ' do
 
+      user = User.create({
+        first_name: 'Nelly',
+        last_name: 'Furtado',
+        email: 'nilly@gmail.com',
+        password: 'thisIsDifferent',
+        password_confirmation: '1234Nilly',
+      })
+
+      expect(user).to_not be_valid
+
+      end
+
+      it 'Should not allow save if passwords do not match' do
+
+        user = User.create({
+          first_name: 'Nelly',
+          last_name: 'Furtado',
+          email: 'nilly@gmail.com',
+          password: 'thisIsDifferent',
+          password_confirmation: '1234Nilly',
+        })
+
+        expect(user.save).to_not be_truthy
+  
+        end
+
+        it 'Should not allow existing emails for registeration -Not case sensitive-' do
+
+          user1 = User.new({
+            first_name: 'Nelly',
+            last_name: 'Furtado',
+            email: 'nilly@gmail.com',
+            password: 'thePasssword',
+            password_confirmation: 'thePasssword',
+          })
+          user1.save
+
+          user2 = User.new({
+            first_name: 'Nelly',
+            last_name: 'Furtado',
+            email: 'NILLY@GMAIL.COM',
+            password: 'thePasssword',
+            password_confirmation: 'thePasssword',
+          })
+          expect(user2.save).to_not be_truthy
+    
+          end
+
+          it 'Should ignore spaces before or after Email input' do
+
+            user = User.new({
+              first_name: 'Nelly',
+              last_name: 'Furtado',
+              email: '  nilly@gmail.com ',
+              password: 'thePasssword',
+              password_confirmation: 'thePasssword',
+            })
+
+            expect(user.save).to be_truthy
+
+      
+          end
+
+          it 'Password should have a minimum length of 5' do
+
+            user = User.new({
+              first_name: 'Nelly',
+              last_name: 'Furtado',
+              email: 'nilly@gmail.com',
+              password: '1234',
+              password_confirmation: '1234',
+            })
+
+            expect(user).to_not be_valid
+
+      
+          end
+  end
+  
 end
