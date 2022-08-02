5affc7411ac490c9656a2ac117d5e21fb0ec46ac
Payam Beigi <beigi.payam@gmail.com>
2022-08-02 07:10:10 +0000

    Add validation specs for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
 
   has_secure_password
-  
+  normalizy :emaile, with: :strip
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :password, length: {minimum: 5}, presence: true
+  validates :password_confirmation, presence: true
+
 end
