efde6da2d292ced14042362cc70005429c74ca37
Payam Beigi <beigi.payam@gmail.com>
2022-08-04 02:43:18 +0000

    Add a new line

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :show]
 
   namespace :admin do
+    
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
